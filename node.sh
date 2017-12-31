287a9cfe52228fa3d4a4a9160556a0e5826a37ae
Hamza Anis <hamzaanis9514@gmail.com>
2017-12-31 09:52:31 -0800

    comments

--- node.sh
+++ node.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+echo -e "Installing Node and Npm"
+
+# Installing build essentials
 apt-get install -y build-essential
 
-echo -e "Installing Node and Npm"
+# Getting the lastest resource.
 
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 apt-get install -y nodejs
 
-
+# Installing npm
 apt-get install npm

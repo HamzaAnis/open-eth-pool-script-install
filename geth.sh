0e4f2c06557e72d8fad0eaf639b95c4cc4f026bc
Hamza Anis <hamzaanis9514@gmail.com>
2018-01-04 22:46:35 +0500

    Geth service

--- geth.sh
+++ geth.sh
@@ -17,7 +17,25 @@ apt-get install ethereum
 if [ "$1" == "--create" ]; then
     geth account new
 fi
+
+echo -e '\033[1;92mMaking a geth service'
+
+echo "[Unit]
+Description=Ethereum Go Client
+[Service]
+ExecStart=/usr/bin/geth --fast --cache=16 --datadir=/mnt/eth-blockchain --identity=@bkawk --keystore=/mnt/eth-blockchain --rpc --rpcport=8882 --rpccorsdomain=* --rpcapi=web3,db,net,eth
+Restart=always
+RestartSec=30
+Type=simple
+User=root
+Group=root
+[Install]
+WantedBy=multi-user.target" > /lib/systemd/system/geth.service
+
+
+systemctl daemon-reload
+systemctl enable geth.service
+
+
 echo -e '\033[1;92mStarting geth'
 screen geth --rpc --fast
-
-

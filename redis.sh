8c699419187bf0077a8aac6b5ac1143647d4fddc
Hamza Anis <hamzaanis9514@gmail.com>
2018-01-04 23:24:58 +0500

    Pool
    Signed-off-by: Hamza Anis <hamzaanis9514@gmail.com>

--- redis.sh
+++ redis.sh
@@ -7,12 +7,13 @@ get_update()
 }
 
 get_update
-echo -e "\033[32mInstalling redis-server"
 
+echo -e "\033[32mInstalling redis-server"
 apt-get install redis-server
 
+# Closing and starting the server if already started
 systemctl restart redis-server.servic e
 
+# Enabling the service on reboot
 echo -e "\033[32mEnabling on reboot"
-
 systemctl enable redis-server.service

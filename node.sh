dbdaacc96531005aec03690ba31c8bab2a74b57f
Hamza Anis <hamzaanis9514@gmail.com>
2018-01-05 01:02:09 +0500

    Fronetend run

--- node.sh
+++ node.sh
@@ -6,7 +6,6 @@ echo -e "Installing Node and Npm"
 apt-get install -y build-essential
 
 # Getting the lastest resource.
-
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 apt-get install -y nodejs
 

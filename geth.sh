8d583a37ac578f9d0fef7f8195849d6a27ca9db1
Hamza Anis <hamzaanis9514@gmail.com>
2018-01-04 17:25:12 +0500

    Geht color

--- geth.sh
+++ geth.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-get_update() 
+
+get_update()
 {
-apt-get update
+    apt-get update
 }
-echo -e "Installing geth"
+echo -e "\033[31mInstalling geth"
 
 apt-get install software-properties-common
 add-apt-repository -y ppa:ethereum/ethereum
@@ -12,3 +13,9 @@ add-apt-repository -y ppa:ethereum/ethereum
 get_update
 
 apt-get install ethereum
+
+if [ "$1" == "--create" ]; then
+    geth account new
+fi
+echo -e '\033[1;91mStarting geth'
+screen geth --rpc --fast

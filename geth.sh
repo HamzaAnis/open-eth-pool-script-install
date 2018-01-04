c2b0ce97aac44f6fc002bd8775bd818da52c951e
Hamza Anis <hamzaanis9514@gmail.com>
2018-01-04 16:58:13 +0500

    Update solved

--- geth.sh
+++ geth.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-sudo apt-get install software-properties-common
-sudo add-apt-repository -y ppa:ethereum/ethereum
-sudo apt-get update
-sudo apt-get install ethereum
+get_update() 
+{
+apt-get update
+}
+echo -e "Installing geth"
+
+apt-get install software-properties-common
+add-apt-repository -y ppa:ethereum/ethereum
+
+get_update
+
+apt-get install ethereum

70d70ab5012d76f5ce8e0e0221408073da114639
Hamza Anis <hamzaanis9514@gmail.com>
2018-01-05 05:43:55 +0500

    COmmn

--- watchman.sh
+++ watchman.sh
@@ -9,4 +9,5 @@ git checkout v4.9.0  # the latest stable release
  make
 sudo make install
 
+# Increasing limit for watchman
 echo 999999 | sudo tee -a /proc/sys/fs/inotify/max_user_watches && echo 999999 | sudo tee -a /proc/sys/fs/inotify/max_queued_events && echo 999999 | sudo tee -a /proc/sys/fs/inotify/max_user_instances && watchman shutdown-server && sudo sysctl -p

5a0c08f24e580384af0b46da0dc1f79c06e79c7d
Hamza Anis <hamzaanis9514@gmail.com>
2018-01-05 05:42:55 +0500

    Increading limit

--- watchman.sh
+++ watchman.sh
@@ -8,3 +8,5 @@ git checkout v4.9.0  # the latest stable release
 ./configure
  make
 sudo make install
+
+echo 999999 | sudo tee -a /proc/sys/fs/inotify/max_user_watches && echo 999999 | sudo tee -a /proc/sys/fs/inotify/max_queued_events && echo 999999 | sudo tee -a /proc/sys/fs/inotify/max_user_instances && watchman shutdown-server && sudo sysctl -p
